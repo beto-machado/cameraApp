4809268cff86bb23768ace8bbb4a41cb513ef33a
Luis Roberto Machado(Beto) <luisrobertobeto3@gmail.com>
2022-05-30 19:39:53 -0300

    Initial commit

---
+++ src/app/tab1/tab1.page.ts
@@ -0,0 +1,19 @@
+import { PhotoService } from './../services/photo.service';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tab1',
+  templateUrl: 'tab1.page.html',
+  styleUrls: ['tab1.page.scss'],
+})
+export class Tab1Page implements OnInit {
+  constructor(public photoService: PhotoService) {}
+
+  addPhotoToGallery() {
+    this.photoService.addNewToGallery();
+  }
+
+  async ngOnInit() {
+    await this.photoService.loadSaved();
+  }
+}
